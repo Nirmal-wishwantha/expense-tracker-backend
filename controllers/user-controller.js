cfb271540a1d9f1f87cfecb019ea0af6af960430
Nirmal-wishwantha <nirmalwishwantha@gmail.com>
2024-10-29 03:48:10 +0530

    user register

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,17 +1,32 @@
-// const connection =require('../dbConnection/db-connection');
-const connection = require('../dbConnection/db-connection')
+const connection = require('../dbConnection/db-connection');
+
+const userRegister = (req, res) => {
+    // Use an array for values
+    connection.query(
+        'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+        [req.body.name, req.body.email, req.body.password],
+        (err) => {
+            if (err) {
+                // Handle error by sending a response
+                console.error('Error inserting user:', err);
+                res.status(500).send('Error registering user');
+            } else {
+                // Successfully registered
+                res.send('User registered!');
+            }
+        }
+    );
+};
 
 
-const userRegister = (reqe, resp) => {
 
-}
 
-const userLogin = (reqe, resp) => {
+const userLogin = (req, res) => {
+    // Implement login logic here
+};
 
-}
+const userGet = (req, res) => {
+    // Implement get user logic here
+};
 
-const userGet = (reqe, resp) => {
-
-}
-
-module.exports = { userRegister, userLogin, userGet }
+module.exports = { userRegister, userLogin, userGet };
